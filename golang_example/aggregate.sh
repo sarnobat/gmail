c3ca1a9f40115ac220427522d0ec3ce1f662867f
Sridhar Sarnobat <ss401533@gmail.com>
2022-12-17 12:29:32 -0800

    Cron daily autocommit (nuc2020

--- golang_example/aggregate.sh
+++ golang_example/aggregate.sh
@@ -1,1 +1,1 @@
-cat output.sarnobat.hotmail.txt | perl -pe 's{::.*}{}g' | perl -pe 's{^.*?\s+}{}g' | perl -pe 's{.*<(.*)>.*}{$1}g' | sort | uniq -c | sort -n | tee /tmp/gmail.txt | tail -100
+cat output.sarnobat.hotmail.txt | perl -pe 's{::.*}{}g' | perl -pe 's{^.*?\s+}{}g' | perl -pe 's{.*<(.*)>.*}{$1}g'  | perl -pe 's{.*@}{@}g' | sort | uniq -c | sort -n
